0c36004cf8877582eef52cc8ec9f64cfb1cbe338
dinesh <dinesh@codingmart.com>
2021-08-11 14:56:45 +0530

    Done Crud with Resttemplate Client

--- src/main/java/com/dinesh/StudentManagementSystem/controller/RestTemplatePractice.java
+++ src/main/java/com/dinesh/StudentManagementSystem/controller/RestTemplatePractice.java
@@ -1,11 +1,10 @@
 package com.dinesh.StudentManagementSystem.controller;
 
+import com.dinesh.StudentManagementSystem.dto.Todo;
 import com.dinesh.StudentManagementSystem.dto.User;
 import com.dinesh.StudentManagementSystem.service.RestTemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/api/v1/rest-template")
@@ -22,5 +21,20 @@ public class RestTemplatePractice {
     @GetMapping("/photo")
     Object[] getPhotos() {
         return service.getPhotos();
+    }
+
+    @PostMapping("/todo")
+    Todo createTodo(@RequestBody Todo todo) {
+        return service.createTodo(todo);
+    }
+
+    @PutMapping("/todo/{id}")
+    Todo updateTodo(@RequestBody Todo todo, @PathVariable int id) {
+        return service.updateTodo(todo, id);
+    }
+
+    @DeleteMapping("/todo/{id}")
+    Todo deleteTodo(@PathVariable int id) {
+        return service.deleteTodo(id);
     }
 }
--- src/main/java/com/dinesh/StudentManagementSystem/service/RestTemplateService.java
+++ src/main/java/com/dinesh/StudentManagementSystem/service/RestTemplateService.java
@@ -1,10 +1,14 @@
 package com.dinesh.StudentManagementSystem.service;
 
+import com.dinesh.StudentManagementSystem.dto.Todo;
 import com.dinesh.StudentManagementSystem.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @Service
 public class RestTemplateService {
@@ -22,5 +26,32 @@ public class RestTemplateService {
 
     public Object[] getPhotos() {
         return restTemplate.getForObject(BASE_URL + "/photos", Object[].class);
+    }
+
+    public Todo createTodo(Todo todo) {
+//        Todo todo = new Todo(1,1,"Todo 1", true);
+        ResponseEntity<Todo> response = restTemplate.postForEntity(BASE_URL + "/photos", todo, Todo.class);
+        return response.getBody();
+    }
+
+    public Todo updateTodo(Todo todo, int id) {
+        System.out.println("todo.getTitle() = " + todo.getTitle());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Todo> entity = new HttpEntity<>(todo,headers);
+        String url = String.format("%s/todos/%s",BASE_URL, id);
+        System.out.println("url = " + url);
+        ResponseEntity<Todo> response = restTemplate.exchange(url, HttpMethod.GET, entity, Todo.class);
+        return response.getBody();
+    }
+
+    public Todo deleteTodo(int id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Todo> entity = new HttpEntity<>(headers);
+        String url = String.format("%s/todos/%s",BASE_URL, id);
+        System.out.println("url = " + url);
+        ResponseEntity<Todo> response = restTemplate.exchange(url, HttpMethod.DELETE, entity, Todo.class);
+        return response.getBody();
     }
 }
