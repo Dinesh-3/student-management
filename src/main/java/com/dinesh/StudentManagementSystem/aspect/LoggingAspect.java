0f43c9255715aef927e864e28e920df8536566c9
dinesh <dinesh@codingmart.com>
2021-08-23 15:41:18 +0530

    AOP practice

---
+++ src/main/java/com/dinesh/StudentManagementSystem/aspect/LoggingAspect.java
@@ -0,0 +1,166 @@
+package com.dinesh.StudentManagementSystem.aspect;
+
+import com.dinesh.StudentManagementSystem.model.Student;
+import com.dinesh.StudentManagementSystem.util.ResponseBody;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(1) // These advices are run first, negative numbers are allowed RANGE: Integer.MIN_VALUE to Integer.MAX_VALUE, if order is same it will pick random order between those
+final public class LoggingAspect {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    // create* (check all the classes methods that are starts with create)
+    // * * createStudent() any modifier and return type
+    // * createStudent() modifier is optional
+
+    // POINT CUT MATCHING METHOD ARGUMENT
+    /*
+    createAccount:
+        () -> 0 arg
+        (${Type}) -> 1 arg with specified type
+        (*) -> 1 arg with any type
+        (..) -> 1 or more arg with any type
+        (${Type}, ..) -> 1 specified type and more args with any type
+     */
+    // com.dinesh.StudentManagementSystem.service.*.*(..) -> matches all the method in service package classes with one or more args
+
+    // DECLARING A POINT CUT
+    /*
+        Uses:
+            1. Easily Reuse point cut logic
+            2. Removes code duplication
+        Add or 1 or more pointcut
+        USING: &&, ||, !
+
+        "controllerPointcut() && !(getter() || setter())" -> In controller applies advice
+            for all the methods excluding getters and setters
+
+     */
+//    @Pointcut("com.dinesh.StudentManagementSystem.service.*.*(..)")
+//    private void servicePointCut(){};
+
+    @Pointcut("execution(* com.dinesh.StudentManagementSystem.controller.*.*(..))")
+    private void controllerPointCut(){};
+
+//    To use Pointcut
+    @Before("com.dinesh.StudentManagementSystem.aspect.Pointcuts.servicePointCut() || controllerPointCut()") // adding multiple point cut
+    public void useServicePointcut(){
+        System.out.println("TO use Pointcut need to add method name with ()");
+    }
+
+//    @Before("execution(public org.springframework.http.ResponseEntity<com.dinesh.StudentManagementSystem.util.ResponseBody> createStudent())")
+//    public void addStudent() {
+//        System.out.println("BEFORE CREATE STUDENT");
+//    }
+
+
+    /*
+        --- JOIN POINTS ---
+        To get method args and more...
+     */
+    @Before("execution(public org.springframework.http.ResponseEntity<com.dinesh.StudentManagementSystem.util.ResponseBody> com.dinesh.StudentManagementSystem.service.StudentService.createStudent(com.dinesh.StudentManagementSystem.model.Student))")
+    public void addStudent(JoinPoint joinPoint) {
+        System.out.println("BEFORE CREATE STUDENT");
+
+        var obj = joinPoint.getArgs()[0];
+        if(obj instanceof Student){
+            Student student = (Student) obj;
+            System.out.println("arg = " + student.getFirst_name());
+            long id = student.getId();
+            System.out.println("student id = " + id);
+        }
+        Signature signature = joinPoint.getSignature();
+        System.out.println("signature = " + signature);
+    }
+
+    @AfterReturning(
+            pointcut = "execution(public org.springframework.http.ResponseEntity<com.dinesh.StudentManagementSystem.util.ResponseBody> com.dinesh.StudentManagementSystem.service.StudentService.createStudent(com.dinesh.StudentManagementSystem.model.Student))",
+            returning = "result" // need to math with below variable name
+    )
+    public void afterReturningStudent(JoinPoint joinPoint, ResponseEntity<ResponseBody> result) {
+        System.out.println("AFTER RETURNING CREATE STUDENT");
+
+//        var obj = joinPoint.getArgs()[0];
+//        if(obj instanceof Student){
+//            Student student = (Student) obj;
+//            System.out.println("arg = " + student.getFirst_name());
+//        }
+//        Signature signature = joinPoint.getSignature();
+//        System.out.println("signature = " + signature);
+        Object data = result.getBody().getData();
+        if(data instanceof Student) {
+            Student student = (Student) data;
+            long id = student.getId();
+            // 🔔 changes will affect in response 🔔
+//            student.setFirst_name("Dinesh I");
+            System.out.println("student id = " + id);
+        }
+
+    }
+
+    /*
+        @AfterThrowing
+
+        Runs if specified aspect throws any error
+     */
+
+    @AfterThrowing(
+            pointcut = "execution(* com.dinesh.StudentManagementSystem.service.*.*(..))",
+            throwing = "exception"
+    )
+    private void afterThrowing(JoinPoint joinPoint, Throwable exception){
+        System.out.println("After Thrown ERROR THROWN AT: " + joinPoint.getSignature().toShortString());
+        System.out.println("Error: " + exception.getMessage());
+//        throw new IllegalArgumentException("Exception thrown in After Thrown");
+    };
+
+    /*
+        @After
+        Runs after method execution runs always success or error thrown similar to finally block
+
+        Points:
+            1. Runs before @AfterThrowing (in this version of spring boot runs after @AfterThrowing)
+     */
+//    @After("execution(* com.dinesh.StudentManagementSystem.service.*.*(..))")
+//    private void afterFinally(JoinPoint joinPoint){
+//        log.info("Method Executed : " + joinPoint.getSignature().toShortString());
+//    };
+
+    /*
+        @Around
+        Combination of @Before and @After
+        Points:
+            1. If Error thrown it will not run
+            2. Runs last after all the advice are executed
+            3. If error thrown first @AfterThrowing runs then catch block run
+     */
+    @Around("execution(* com.dinesh.StudentManagementSystem.service.*.*(..))")
+    private Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = null;
+        try {
+            result = joinPoint.proceed();
+        } catch (Throwable e) {
+            System.out.println("EXCEPTION HANDLING IN AROUND Error: " + e.getMessage());
+            throw e;
+        }
+        long end = System.currentTimeMillis();
+        long timeInMillis = end - start;
+        System.out.println("Execution Time in Millis: " + timeInMillis);
+
+        // if we change the return type to primitive or different type
+        // except original and Object types it will throw type cast error
+        // in controller so be careful
+        return result;
+    };
+
+}
---
+++ src/main/java/com/dinesh/StudentManagementSystem/aspect/Pointcuts.java
@@ -0,0 +1,24 @@
+package com.dinesh.StudentManagementSystem.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class Pointcuts {
+
+    /*
+        By this we can use these pointcuts anywhere in the project
+
+        In order to use these pointcuts need to add fully qualified name
+        like below:
+             @Before("com.dinesh.StudentManagementSystem.aspect.Pointcuts.servicePointCut()")
+
+     */
+
+    @Pointcut("execution(* com.dinesh.StudentManagementSystem.service.*.*(..))")
+    public void servicePointCut(){};
+
+    @Pointcut("execution(* com.dinesh.StudentManagementSystem.controller.*.*(..))")
+    public void controllerPointCut(){};
+
+}
