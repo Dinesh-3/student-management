700f6af176d21d283386411b355137a52f80ed2c
dinesh <dinesh@codingmart.com>
2021-08-24 13:58:31 +0530

    add profiles

--- src/main/java/com/dinesh/StudentManagementSystem/StudentManagementSystemApplication.java
+++ src/main/java/com/dinesh/StudentManagementSystem/StudentManagementSystemApplication.java
@@ -3,11 +3,14 @@ package com.dinesh.StudentManagementSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+//@RequestMapping("api/v1") not work
 public class StudentManagementSystemApplication {
 
+//	@RequestMapping("api/v1") not work
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
--- src/main/java/com/dinesh/StudentManagementSystem/controller/LogController.java
+++ src/main/java/com/dinesh/StudentManagementSystem/controller/LogController.java
@@ -4,10 +4,8 @@ import com.dinesh.StudentManagementSystem.service.LogService;
 import com.dinesh.StudentManagementSystem.util.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/log")
